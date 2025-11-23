8537727917ec9a994130e58be5fe5d96622b894a
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-11-22 19:19:14 -0700

    Added Pie Chart

--- main.py
+++ main.py
@@ -9,57 +9,22 @@ def main():
     data = pd.read_csv("data.csv")
     fig, ax = plt.subplots()
 
-    # ----------------------------
-    # Generate Example Data
-    # ----------------------------
-    # Data for bar and line charts
-    x = np.arange(5)
-    y = np.array([10, 23, 15, 7, 30])
+    print(data["Crunchiness"] > data["Juiciness"])
+    good_apples = data[data["Quality"] == "good"]
+    bad_apples = data[data["Quality"] == "bad"]
+    pie_chart(ax, [len(good_apples), len(bad_apples)], ["Good", "Bad"])
+    plt.show()
 
-    # Data for pie chart
-    sizes = np.array([40, 25, 20, 10, 5])
-    labels = ["Apples", "Bananas", "Cherries", "Dates", "Elderberries"]
+    # # 4. Histogram
+    histogram(ax, good_apples["Size"], number_of_bins=20)
+    set_titles(ax, title="Good Apples", x_label="Size", y_label="Frequency")
+    # save_plot(fig, "histogram_good.png")
+    # plt.cla()
 
-    # Data for histogram
-    hist_data = np.random.normal(loc=50, scale=10, size=500)
-
-    # Data for scatter plot
-    x_scatter = np.random.rand(100)
-    y_scatter = 2 * x_scatter + np.random.randn(100) * 0.1  # linear with noise
-
-    # ----------------------------
-    # Create Plots
-    # ----------------------------
-
-    # 1. Bar Chart
-    bar_chart(ax, x, y)
-    set_titles(ax, x_label="Category", y_label="Value", title="Bar Chart")
-    save_plot(fig, "bar_chart.png")
-    plt.cla()
-
-    # 2. Line Chart
-    line_chart(ax, x, y)
-    set_titles(ax, x_label="Index", y_label="Value", title="Line Chart")
-    save_plot(fig, "line_chart.png")
-    plt.cla()
-
-    # 3. Pie Chart
-    pie_chart(ax, percentages=sizes, labels=labels)
-    set_titles(ax, title="Pie Chart")
-    save_plot(fig, "pie_chart.png")
-    plt.cla()
-
-    # 4. Histogram
-    histogram(ax, hist_data, number_of_bins=20)
-    set_titles(ax, title="Histogram", x_label="Value", y_label="Frequency")
+    histogram(ax, bad_apples["Size"], number_of_bins=20)
+    set_titles(ax, title="Bad Apples", x_label="Size", y_label="Frequency")
     save_plot(fig, "histogram.png")
-    plt.cla()
-
-    # 5. Scatter Plot
-    scatter_plot(ax, x_scatter, y_scatter)
-    set_titles(ax, x_label="X Values", y_label="Y Values", title="Scatter Plot")
-    save_plot(fig, "scatter_plot.png")
-    plt.cla()
+    plt.show()
 
 
 if __name__ == "__main__":
